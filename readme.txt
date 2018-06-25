c5071ae790d50fe545bb58a49c1b695f5ebaedd2
Johnson Fu <johnson.fu@gmail.com>
2018-06-25 13:40:05 +0800

    test spring security

---
+++ readme.txt
@@ -0,0 +1,26 @@
+
+http://localhost:8080/login
+[POST] sign-up JSON:
+{
+	"username":"admin",
+	"password":"password"
+}
+
+
+http://localhost:8080/users/sign-up
+[POST] login JSON
+{
+	"username":"admin",
+	"password":"password"
+}
+
+
+http://localhost:8080/tasks
+[POST] add task
+{
+	"description": "apple"
+}
+
+
+http://localhost:8080/tasks
+[GET] query all tasks
--- src/main/java/foosi/authapi/security/WebSecurity.java
+++ src/main/java/foosi/authapi/security/WebSecurity.java
@@ -16,7 +16,13 @@ import static foosi.authapi.security.SecurityConstants.SIGN_UP_URL;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * primary configuration for the spring security
+ *
+ */
+@Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
@@ -30,18 +36,39 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll() // permit only sign up URL
+                .antMatchers(HttpMethod.POST, "/welcome").permitAll()
+                .antMatchers(HttpMethod.GET, "/welcome").permitAll()
+                .antMatchers(HttpMethod.POST, "/").permitAll()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 // this disables session creation on Spring Security
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().formLogin()
+                	//.loginPage("/log").permitAll() // for custom login page
+                ;
+        
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	
+    	// 1. set the user details service which is used to return the user detail with name and the encoded password
+    	// 2. tell the user details service that which password encoder is used
+    	
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
+    
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	  auth.inMemoryAuthentication().withUser("test").password("123456").roles("USER");
+	  auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+	  auth.inMemoryAuthentication().withUser("dba").password("123456").roles("DBA");
+	}    
 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
